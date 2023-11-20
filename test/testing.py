48ee36fc370e73c49cd66b59138d57f52e3b7144
bonenaga <133274395+bonenaga@users.noreply.github.com>
2023-11-19 17:59:42 -0700

    Add files via upload

--- test/testing.py
+++ test/testing.py
@@ -1,20 +1,23 @@
+import sys
+import os
+import shutil
+import unittest
+import argparse
+import importlib
 
-import sys, os, shutil, unittest
-import optparse
 
-
-class OptionParser (optparse.OptionParser):
+class OptionParser(argparse.ArgumentParser):
     def __init__(self, *args):
-        optparse.OptionParser.__init__(self, *args)
+        argparse.ArgumentParser.__init__(self, *args)
 
     def exit(self, code, text):
         pass
 
 
-
 def clean_dir(path):
     if os.path.exists(path):
         shutil.rmtree(path)
+
 
 def makedirs(path):
     if not os.path.exists(path):
@@ -28,31 +31,33 @@ def make_clean_dir(path):
 
 
 def list_tests(stack=0):
-    
     # get environment
-    var = __import__("__main__").__dict__
+    var = importlib.import_module("__main__").__dict__
 
-    for name, obj in var.iteritems():
+    for name, obj in var.items():
         if isinstance(obj, type) and issubclass(obj, unittest.TestCase):
             for attr in dir(obj):
                 if attr.startswith("test"):
-                    print "%s.%s" % (name, attr),
+                    print("%s.%s" % (name, attr), end=" ")
                     doc = getattr(obj, attr).__doc__
                     if doc:
-                        print "--", doc.split("\n")[0]
+                        print("--", doc.split("\n")[0])
                     else:
-                        print
+                        print()
 
 
 def test_main():
-
     o = OptionParser()
-    o.add_option("-l", "--list_tests", action="store_true")
+    o.add_argument("-l", "--list_tests", action="store_true")
 
-    conf, args = o.parse_args(sys.argv)
+    conf, args = o.parse_known_args()
 
     if conf.list_tests:
         list_tests(1)
         return
 
     unittest.main()
+
+
+if __name__ == "__main__":
+    test_main()
